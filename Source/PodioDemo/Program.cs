c50fd3ec90b90924b5f593ad9a8f361a67e20426
Matt Riedel <dev6@sovren.com>
2018-03-06 21:24:21 -0500

    initial commit

---
+++ Source/PodioDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using PodioAPI;
+using PodioAPI.Utils;
+
+namespace PodioDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var currentIp = GetCurrentIpAddress();
+            Console.WriteLine($"Your current IP is: {currentIp}");
+
+            var appId = 0;
+            var podio = new Podio("clientId", "clientSecret");
+            podio.AuthenticateWithApp(appId, "appSecret");
+            
+            
+            var items = podio.ItemService.FilterItems(appId);
+            Console.WriteLine("My app has " + items.Total + " items");
+
+
+            Console.ReadLine();
+        }
+
+
+
+        public static string GetCurrentIpAddress()
+        {
+            return new WebClient().DownloadString("http://icanhazip.com");
+        }
+    }
+}
